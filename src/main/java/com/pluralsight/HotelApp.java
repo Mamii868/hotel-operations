8b6041848861e3b3b914dd6b518bba82a0c281ee
Mamii868 <marcmotenii@gmail.com>
2026-05-04 14:52:54 -0500

    Print test

--- src/main/java/com/pluralsight/HotelApp.java
+++ src/main/java/com/pluralsight/HotelApp.java
@@ -2,6 +2,12 @@ package com.pluralsight;
 
 public class HotelApp {
     public static void main(String[] args) {
+        Room room = new Room(4, 128.00, true, false);
+
+        Reservation reservation = new Reservation("King", 6, false);
+
+        System.out.println(room.isAvailable());
+        System.out.printf("$%.2f", reservation.getReservationTotal());
 
     }
 }
