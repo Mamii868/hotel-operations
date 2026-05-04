b064def677ff86cce5bd457bd3d2c6d7ae8fc813
Mamii868 <marcmotenii@gmail.com>
2026-05-04 14:46:12 -0500

    Add reservation class

---
+++ src/main/java/com/pluralsight/Reservation.java
@@ -0,0 +1,51 @@
+package com.pluralsight;
+
+public class Reservation {
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+    public double getPrice() {
+        double price = 0;
+        if (this.roomType.equalsIgnoreCase("king")) {
+            price += 139;
+        } else if (this.roomType.equalsIgnoreCase("double")) {
+            price += 124;
+        }
+        return price;
+    }
+
+    public double getReservationTotal() {
+        return getPrice() * this.numberOfNights;
+    }
+}
