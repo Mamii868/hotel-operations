892b038a68d0dc5fb2fef2abee9198f377603130
Mamii868 <marcmotenii@gmail.com>
2026-05-05 14:07:02 -0500

    Add hotel class

---
+++ src/main/java/com/pluralsight/Hotel.java
@@ -0,0 +1,43 @@
+package com.pluralsight;
+
+public class Hotel {
+    private String name;
+    private int numberOfSuites;
+    private int numberOfRooms;
+    private int bookedSuites;
+    private int bookedBasicRoomes;
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = 0;
+        this.bookedBasicRoomes = 0;
+    }
+
+    public boolean bookRoom(int numberOfRooms, boolean isSuite) {
+        if (isSuite) {
+            if (this.numberOfSuites - this.bookedSuites >= numberOfRooms) {
+                this.bookedSuites += numberOfRooms;
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (this.numberOfRooms - this.bookedBasicRoomes >= numberOfRooms) {
+                this.bookedBasicRoomes += numberOfRooms;
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public int getAvailableRooms() {
+        return this.numberOfRooms - this.bookedBasicRoomes;
+    }
+
+    public int getAvailableSuites() {
+        return this.numberOfSuites - this.bookedSuites;
+    }
+}
