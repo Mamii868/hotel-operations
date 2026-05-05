4efd017c07001180a1fc66d2e2a38b6694c2d933
Mamii868 <marcmotenii@gmail.com>
2026-05-05 11:14:35 -0500

    Add check in, out, and clean methods

--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -32,4 +32,37 @@ public class Room {
     public boolean isAvailable() {
         return !this.isOccupied && !this.isDirty;
     }
+
+    public void checkIn() {
+        if (this.isAvailable()) {
+            this.isDirty = true;
+            this.isOccupied = true;
+            System.out.println("Guest is checked in!");
+        } else if (this.isDirty && !this.isOccupied) {
+            System.out.println("Room is still dirty!");
+        } else if (this.isOccupied) {
+            System.out.println("Room is occupied!");
+        }
+    }
+
+    public void checkOut() {
+//        Occupied rooms will always be dirty so no need to check for it
+        if (this.isOccupied()) {
+            this.isOccupied = false;
+            System.out.println("Guest Checked out!");
+            cleanRoom();
+        } else {
+            System.out.println("Guest already checked out!");
+        }
+    }
+
+    public void cleanRoom() {
+        if (!this.isOccupied()) {
+            this.isDirty = false;
+            System.out.println("Room has been cleaned!");
+        } else {
+            System.out.println("Room is still occupied!");
+        }
+    }
+
 }
