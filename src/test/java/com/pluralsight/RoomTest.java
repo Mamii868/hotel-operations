c9629c1c48bbdf859f18b65b74f0c26f55b208dc
Mamii868 <marcmotenii@gmail.com>
2026-05-06 14:02:57 -0500

    Split tests

--- src/test/java/com/pluralsight/RoomTest.java
+++ src/test/java/com/pluralsight/RoomTest.java
@@ -7,24 +7,41 @@ import static org.junit.jupiter.api.Assertions.*;
 class RoomTest {
 
     @Test
-    public void checkIn_should_markRoomDirtyAndOccupied() {
+    public void checkIn_should_markRoomDirty() {
         Room room = new Room(2, 125.00, false, false);
 
         room.checkIn();
 
         assertTrue(room.isDirty());
+    }
+
+    @Test
+    public void checkIn_should_markRoomOccupied() {
+        Room room = new Room(2, 125.00, false, false);
+
+        room.checkIn();
+
         assertTrue(room.isOccupied());
     }
 
     @Test
-    public void checkOut_should_markRoomCleanAndNotOccupied() {
+    public void checkOut_should_markRoomClean() {
+        Room room = new Room(2, 125.00, false, false);
+
+        room.checkIn();
+        room.checkOut();
+
+        assertFalse(room.isDirty());
+    }
+
+    @Test
+    public void checkOut_should_markRoomNotOccupied() {
         Room room = new Room(2, 125.00, false, false);
 
         room.checkIn();
         room.checkOut();
 
         assertFalse(room.isOccupied());
-        assertFalse(room.isDirty());
     }
 
     @Test
